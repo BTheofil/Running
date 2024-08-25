53b48e3f98a77a42dcd62157040de01cf2457e83
BTheofil <btheofil7@gmail.com>
2024-08-25 20:33:39 +0200

    add run ui models

---
+++ run/presentation/src/main/java/hu/tb/run/presentation/run_overview/mapper/RunMapper.kt
@@ -0,0 +1,32 @@
+package hu.tb.run.presentation.run_overview.mapper
+
+import hu.tb.core.domain.run.Run
+import hu.tb.core.presentation.ui.formatted
+import hu.tb.core.presentation.ui.toFormattedKm
+import hu.tb.core.presentation.ui.toFormattedKmh
+import hu.tb.core.presentation.ui.toFormattedMeters
+import hu.tb.core.presentation.ui.toFormattedPace
+import hu.tb.run.presentation.run_overview.model.RunUi
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+fun Run.toRunUi(): RunUi {
+    val dateTimeInLocalTime = dateTimeUtc
+        .withZoneSameInstant(ZoneId.systemDefault())
+    val formattedDateTime = DateTimeFormatter
+        .ofPattern("MMM dd, yyyy - hh:mma")
+        .format(dateTimeInLocalTime)
+
+    val distanceKm = distanceMeters / 1000.0
+    return RunUi(
+        id = id!!,
+        duration = duration.formatted(),
+        dateTime = formattedDateTime,
+        distance = distanceKm.toFormattedKm(),
+        avgSpeed = avgSpeedKmh.toFormattedKmh(),
+        maxSpeed = maxSpeedKmh.toFormattedKmh(),
+        pace = duration.toFormattedPace(distanceKm),
+        totalElevation = totalElevationMeters.toFormattedMeters(),
+        mapPictureUrl = mapPictureUrl
+    )
+}
